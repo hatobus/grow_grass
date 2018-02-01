c97ce218291a326ef2e959937f08f9bc720acfda
hatobus <s1240056@gmail.com>
2018-02-01 18:51:21 +0900

    update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # grow_grass
+
+Grow grass is the command, to realize the internet slang "草" or "草生えた".
+
+Japanese "otaku" often to use word "草".
+
+This is same as a "LoL".
---
+++ a.sh
@@ -0,0 +1,6 @@
+echo "# grow_grass" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/hatobus/grow_grass.git
+git push -u origin master
